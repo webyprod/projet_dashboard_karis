adde0913b67762b620ffd30c40c6d708697f8f2c
fox <mk.alex1989@gmail.com>
2016-07-04 21:39:46 +0200

    Creation fichier smrush qui servira a la connexion auto sur Semrush

---
+++ smrush.py
@@ -0,0 +1,8 @@
+#!/usr/local/bin/python3.3
+
+### surnom : Fox
+### date : 29 Janvier 2016
+### programme : smrush.py
+### Objectif : Cette classe utilisera Selenium pour automatiser une connexion
+###            sur Semrush, fera une impression d'ecran du tableau de bord de Karis
+###            puis ferme l'application
