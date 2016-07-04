cd7c5293f967c2ab0802ca52e00d2fc7bb02c742
fox <mk.alex1989@gmail.com>
2016-07-04 22:02:13 +0200

    Application main()

--- prog_main_karis.py
+++ prog_main_karis.py
@@ -7,3 +7,43 @@
 ###            se connecter a Semrush automatique et enregistrer le tableau de bord
 ###            puis enverra un message a ma boite mail pour me dire que j'ai recu
 ###            le tableau de bord.
+
+
+import smrush as sem
+import mail
+
+def main():
+  #  Instanciation classe Semrush
+  karis = sem.Semrush()
+
+  # Navigation vers page d'accueil Semrush puis clique sur Connexion
+  karis.home_page()
+
+  # Connexion Compte client  
+  karis.connexion()
+
+  # Retrouve le lien du site Karis Formation
+  karis.tableauDeBord()
+
+  # Impression d'ecran et enregistrement sur disque dur
+  karis.impressionEcran()
+
+  
+  karis.close()
+
+
+  #  Instanciation classe Message
+  message = mail.Message()
+
+  #  Connexion Serveur SMTP
+  message.connexion()
+
+  #  Envoi de message 
+  message.envoi_message("Vous trouverez le rapport des positions de Karis Formation dans le dossier '/home/darius/karis.jpg' Merci")
+
+  message.quit()
+
+
+
+if __name__ == "__main__":
+  main()
