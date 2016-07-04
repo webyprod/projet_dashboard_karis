114d44539f5aa0c58f20bb1689ee84289d1806a9
fox <mk.alex1989@gmail.com>
2016-07-04 21:51:59 +0200

    Creation fichier principal main

---
+++ prog_main_karis.py
@@ -0,0 +1,9 @@
+#!/usr/local/bin/python3.3
+
+### surnom : Fox
+### date : 29 Janvier 2016
+### programme : prog_main_karis.py
+### Objectif : programme principal qui utilisera les deux classes pour d'une part
+###            se connecter a Semrush automatique et enregistrer le tableau de bord
+###            puis enverra un message a ma boite mail pour me dire que j'ai recu
+###            le tableau de bord.
