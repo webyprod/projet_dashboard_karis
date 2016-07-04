163de99db6d00bf2dfa339feb78373194da0fdf5
fox <mk.alex1989@gmail.com>
2016-07-04 21:48:26 +0200

    Creation Classe Message specialise dans l'envois de mail

--- mail.py
+++ mail.py
@@ -5,3 +5,25 @@
 ### programme : mail.py
 ### Objectif : Cette classe utilisera le module smtplib pour l'envoi du mail
 
+import smtplib
+
+
+class Message:
+  def __init__(self):
+    # Preparation connexion Google Mail
+    self.messagerie = smtplib.SMTP('smtp.gmail.com:587')
+    self.messagerie.starttls()
+    self.expediteur = "mk.alex1989@gmail.com"
+    self.destinataire = "a.mokadem@forma-dis.com"
+
+  def connexion(self):
+    # Connexion avec mes identifiants mail + mdp
+    self.messagerie.login('mk.alex1989@gmail.com','************')
+
+  def envoi_message(self, mess=""):
+    # Envoi du mail
+    self.messagerie.sendmail(self.expediteur, self.destinataire, mess)
+
+  def quit(self):
+    # Fermeture de l'application
+    self.messagerie.quit()
